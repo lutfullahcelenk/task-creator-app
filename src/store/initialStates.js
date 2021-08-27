e26fe5c0df0a3a12faa1bd738592050b0f12dd68
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-27 10:09:16 +0300

    initialStates are uploaded from json file to use default states in the app

---
+++ src/store/initialStates.js
@@ -0,0 +1,20 @@
+export const initialState = [
+    {
+      id: 1,
+      text: "Study React Pre-Class Notes",
+      day: "Feb 5th at 2:30pm",
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: "Feed the Dog",
+      day: "Feb 6th at 1:30pm",
+      isDone: false,
+    },
+    {
+      id: 3,
+      text: "Attend in-Class",
+      day: "Feb 7th at 20:00pm",
+      isDone: false,
+    },
+  ];
