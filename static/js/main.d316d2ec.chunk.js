061c0ad6977df12ca5a81608a72b74e235d4ebe3
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-27 10:18:20 +0300

    Updates

---
+++ static/js/main.d316d2ec.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptask-creator-app"]=this["webpackJsonptask-creator-app"]||[]).push([[0],{10:function(t,r,e){"use strict";e.r(r);var c=e(3),n=e.n(c),a=(e(1),e(8),e(0));var o=function(){return Object(a.jsx)("div",{})};n.a.render(Object(a.jsx)(o,{}),document.querySelector("#root"))},8:function(t,r,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.d316d2ec.chunk.js.map
