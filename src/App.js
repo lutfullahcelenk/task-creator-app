8fddae8a8f04cfe044cd2787631a6d6f34cfc496
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-27 10:24:00 +0300

    Header components is created and now we will add Button component into Header as a child component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./App.css";
+import Header from './components/Header';
 
 function App() {
     return (
-        <div>
-            
+        <div className="container">
+
+            <Header 
+                title = "Task Creator"
+                
+            />
+
         </div>
     )
 }
